b2d6eac290fc10002d489740ef630fb9ee2cfa7d
Moon YuBin <yubin5744@gmail.com>
2025-10-23 23:47:17 +0900

    late variables

--- main.dart
+++ main.dart
@@ -1,7 +1,11 @@
 void main() {
 
-  // final 키워드로 선언하면 변수 업데이트 불가
-  final name = "yubin";
-  // name = "moon";
+  // late 키워드는 변수에 값을 담지 않고 미리 선언할 수 있게 해줌.
+  // 후에 값을 넣기 때문에 late
+  late final String name;
+  name = "yubin"; // final이라 한 번만 할당 가능
+  print(name);
+
+  // API date fetching에 자주 쓰임
 
 }
