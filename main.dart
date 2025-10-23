568b3a3a89bde14bbbe940f0d2e2cf113760ad8f
Moon YuBin <yubin5744@gmail.com>
2025-10-23 23:28:54 +0900

    nullable variables

--- main.dart
+++ main.dart
@@ -1,14 +1,20 @@
 void main() {
 
-    // 따로 타입을 지정하지 않는 dynamic
-    dynamic name;
-    if (name is String) {
-      // 조건문으로 타입을 확인한 후에는 
-      // 해당 변수가 그 타입이 되고 타입에 따라 다양한 메소드 사용 가능
-    }
+  // 기본적으로 모든 variable은 not nullable
+  // 그러나 타입 뒤에 ?를 붙이면 nullable
+  String? yubin = "yubin";
+  yubin = null;
 
-    if (name is int) {
-      bool isEven = name.isEven;
-    }
+  // 조건문으로 확인 후에는
+  // 조건문 블록 안에서는 null이 아님
+  if (yubin != null) {
+    yubin.length;
+    yubin.isNotEmpty;
+  }
+
+  // 간단하게는 이렇게 표현 가능 
+  // yubin 변수가 null이 아니라면 .length 메서드를 사용한다는 뜻
+  yubin?.length;
+  yubin?.isNotEmpty;
 
 }
