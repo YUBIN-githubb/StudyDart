1ddd7b72765db79a69f0215da02eef2bda6feceb
Moon YuBin <yubin5744@gmail.com>
2025-10-23 23:02:58 +0900

    the var keyword

--- main.dart
+++ main.dart
@@ -1,3 +1,11 @@
 void main() {
-    print("hello world!!!!");
+
+    // 메소드 내부의 지역변수에는 var
+    var name = "유빈";    
+
+    // 데이터 타입을 지켜준다면 var 타입으로 선언 후 업데이트 가능
+    String stringName = "유빈";
+
+    print(stringName);
+
 }
