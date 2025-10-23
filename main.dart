b7d15b8e6a01ef126eb2942c78a56caf900ffb9d
Moon YuBin <yubin5744@gmail.com>
2025-10-23 23:14:17 +0900

    dynamic type

--- main.dart
+++ main.dart
@@ -1,11 +1,14 @@
 void main() {
 
-    // 메소드 내부의 지역변수에는 var
-    var name = "유빈";    
+    // 따로 타입을 지정하지 않는 dynamic
+    dynamic name;
+    if (name is String) {
+      // 조건문으로 타입을 확인한 후에는 
+      // 해당 변수가 그 타입이 되고 타입에 따라 다양한 메소드 사용 가능
+    }
 
-    // 데이터 타입을 지켜준다면 var 타입으로 선언 후 업데이트 가능
-    String stringName = "유빈";
-
-    print(stringName);
+    if (name is int) {
+      bool isEven = name.isEven;
+    }
 
 }
