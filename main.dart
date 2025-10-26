aa2bca331d36acd6960f6defd3d547d393e97843
Moon YuBin <yubin5744@gmail.com>
2025-10-26 20:39:02 +0900

    collection for

--- main.dart
+++ main.dart
@@ -1,8 +1,11 @@
 void main() {
 
-  // string interpolation
-  var name = 'yubin';
-  var age = 10;
-  var greeting = 'Hello everyone my name is $name and I am ${age +2}.';
-  print(greeting);
+  var oldFriends = ['철수', '유리'];
+  var newFriends = [
+    '지수',
+    '훈이',
+    for (var friend in oldFriends) "♥️ $friend"
+    ];
+
+  print(newFriends);
 }
