9a24644f56fffb2bbb159226f27c6b78e9aaeb06
Moon YuBin <yubin5744@gmail.com>
2025-10-26 20:32:40 +0900

    string interpolation

--- main.dart
+++ main.dart
@@ -1,11 +1,8 @@
 void main() {
 
-  // collection if
-  // if giveMeFive is ture then add 5
-  var giveMeFive = true;
-  var numbers = [1, 2, 3, if (giveMeFive) 5,];
-  numbers.add(10);
-  numbers.first;
-  numbers.last;
-  print(numbers);
+  // string interpolation
+  var name = 'yubin';
+  var age = 10;
+  var greeting = 'Hello everyone my name is $name and I am ${age +2}.';
+  print(greeting);
 }
