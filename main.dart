cf4a82f6e5cf052ce5a3cfe2f17ba2e259462847
Moon YuBin <yubin5744@gmail.com>
2025-10-26 16:40:18 +0900

    basic data types

--- main.dart
+++ main.dart
@@ -1,11 +1,8 @@
 void main() {
 
-  // const 키워드는 compile-time에 알고 있는 값
-  // 하드코딩되어 잆는 값. 상수
-  const APi_KEY = "dfaewr";
-  const max_allowed_price = 120;
-
-  // 컴파일 타임에 어떤 값인지 모르고 혹은 그 값이 API로부터 온다거나
-  // 사용자가 화면에서 입력하는 값이라면 var 이나 final 이어야 함
-
+  // basic data types
+  String name = "yubin";
+  bool alive = true;
+  int age = 10;
+  double money = 9.99;
 }
