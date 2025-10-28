8f78a47ed93564bb4f32aa6455e05fe58b342f79
Moon YuBin <yubin5744@gmail.com>
2025-10-28 13:33:23 +0900

    defining a function

--- main.dart
+++ main.dart
@@ -1,9 +1,9 @@
+// fat arrow syntax == immediately return 
+String sayHello(String name) => "Hello $name nice to meet you!";
+
+int plus(int a, int b) => a + b;
+
 void main() {
 
-  // all items in set is unique
-  var numbers = {1,2,3,4};
-  numbers.add(1);
-  numbers.add(1);
-  numbers.add(1);
-  print(numbers);
+  print(sayHello("yubin"));
 }
