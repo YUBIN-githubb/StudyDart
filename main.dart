10aef83fb6468c61436f20833202f24268e8f3cc
Moon YuBin <yubin5744@gmail.com>
2025-10-28 14:20:18 +0900

    ??=

--- main.dart
+++ main.dart
@@ -1,7 +1,6 @@
-// 왼쪽 결과물이 null 아니라면 그대로 왼쪽 return. null이라면 오른쪽 return
-String capitalizeName(String? name) => name?.toUpperCase() ?? 'anonymus';
-
 void main() {
-  capitalizeName('yubin');
-  capitalizeName(null);
+  String? name;
+  // if name is null then put this value instead of it.
+  name ??= 'yubin';
+  print(name);
 }
