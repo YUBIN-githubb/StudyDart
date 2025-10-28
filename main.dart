e58800aa5d493b56c4e2ed3a225542cf57b9c055
Moon YuBin <yubin5744@gmail.com>
2025-10-28 14:07:01 +0900

    optional positional parameter

--- main.dart
+++ main.dart
@@ -1,17 +1,9 @@
-// add curly bracket to parameter
-// required == always to be colled with parameter value
-String sayHello({
-  required String name, 
-  required int age, 
-  required String country}) {
-  return "Hello $name, you are $age, and you come from $country";
-}
+String sayHello(
+  String name, 
+  int age, 
+  [String? country = 'wakanda']) => "Hello $name, you are $age and you come from $country";
 
 void main() {
-
-  print(sayHello(
-    age: 10,
-    country: 'Korea',
-    name: 'yubin'
-  ));
+  var results = sayHello('yubin', 10);
+  print(results);
 }
