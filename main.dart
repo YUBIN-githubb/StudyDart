057b6c8df3e33b3aa8c72f2613fb6ce5746106f2
Moon YuBin <yubin5744@gmail.com>
2025-10-31 21:00:25 +0900

    constructor

--- main.dart
+++ main.dart
@@ -1,14 +1,17 @@
 class Player {
-  final String name = 'yubin';
-  int xp = 1000;
+  final String name;
+  int xp;
+
+  Player(this.name, this.xp); // position 중요함
 
   void sayHello() {
-    print("Hello. My name is $name"); // 함수 내에 property와 겹치는 변수 이름이 있지 않은 이상 this 사용 권고X
+    print("Hello. My name is $name"); 
   }
 }
 
 void main() {
-  var player = Player(); // no need new keyword
-  print(player.name);
+  var player = Player('yubin', 1000); 
+  var player2 = Player('bogsil', 5000);
   player.sayHello();
+  player2.sayHello();
 }
